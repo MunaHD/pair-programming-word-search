0636476a5a5369520b301c94e0a336ff175ef45b
Muna Dahir <muna.dahir@hotmail.com>
2021-11-03 16:37:14 -0400

    made the wordsearch program run correctly with some tests

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    if (word === []) return false;
+
+    let returnValue = false
+    for (let l of horizontalJoin) {
+        
+        if (l.includes(word)) returnValue = true; 
+    } 
+    
+    let verticalJoin = ''
+    for (let i = 0; i < letters[0].length; i++) {
+        for (let j = 0; j < letters.length; j++) {
+         verticalJoin = verticalJoin + letters[j][i];
+        }
+        if (verticalJoin.includes(word)) returnValue = true;
     }
+
+    return returnValue;
+    
+
 }
 
 module.exports = wordSearch
